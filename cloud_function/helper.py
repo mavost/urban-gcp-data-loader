6bd85da7953f5bf14d3c2aa3d73449a2247081cb
Markus von Steht <markus.von.steht@gmx.de>
2025-05-06 08:26:40 +0200

    switched to HTTP func trigger, offset loading, writing JSON-NL

--- cloud_function/helper.py
+++ cloud_function/helper.py
@@ -1,15 +1,40 @@
 import requests
+import logging
+from datetime import datetime, timezone
 
-def fetch_paginated_data(base_url):
-    all_data = []
-    page = 1
-    while True:
-        response = requests.get(f"{base_url}?page={page}")
-        if response.status_code != 200:
-            raise Exception(f"Failed to fetch data: {response.text}")
-        data = response.json()
-        if not data:
-            break
-        all_data.extend(data)
-        page += 1
-    return all_data
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# def fetch_paginated_data(base_url):
+#   all_data = []
+#   page = 1
+#   while True:
+#     response = requests.get(f"{base_url}?page={page}")
+#     if response.status_code != 200:
+#       raise Exception(f"Failed to fetch data: {response.text}")
+#     data = response.json()
+#     if not data:
+#       break
+#     all_data.extend(data)
+#     page += 1
+#   return all_data
+
+def fetch_offset_data(base_url):
+  all_data = []
+  limit = 100
+  offset = 0
+  max = 500
+
+  current_ts = datetime.now(timezone.utc).strftime('%Y%m%d000000')
+  logger.info(f"Fetching data for {current_ts}")
+  while offset < max:
+    response = requests.get(f"{base_url}?$limit={limit}&$offset={offset}")
+    if response.status_code != 200:
+      raise Exception(f"Failed to fetch data: {response.text}")
+    logger.info(f"Fetching offset: {offset}")
+    data = response.json()
+    if not data:
+      break
+    all_data.extend(data)
+    offset += limit
+  return all_data
--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,18 +1,42 @@
 import json
-from datetime import datetime
+import os
+
+from datetime import datetime, timezone
 from google.cloud import storage
-from helper import fetch_paginated_data
+import functions_framework
+# transitive dependency
+from flask import Request
+from helper import fetch_offset_data
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@functions_framework.http
+def ingest_api_data(request: Request):
+
+  BASE_URL = os.getenv("BASE_URL")
+  #BASE_URL = "https://data.ny.gov/resource/xyvi-fbb9.json"
+  if not BASE_URL:
+    raise ValueError("Environment variable BASE_URL is not set")
+
+  BUCKET_NAME = os.getenv("BUCKET_NAME")
+  if not BUCKET_NAME:
+    raise ValueError("Environment variable BUCKET_NAME is not set")
+
+  try:
+    logger.info("Fetching data...")
+    data = fetch_offset_data(BASE_URL)
+    upload_to_gcs(BUCKET_NAME, data)
+    return ("Data uploaded successfully", 200)
+  except Exception as e:
+    return (f"Error occurred: {str(e)}", 500)
 
 def upload_to_gcs(bucket_name, data):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    date_str = datetime.utcnow().strftime('%Y-%m-%d')
-    blob = bucket.blob(f"data/{date_str}.json")
-    blob.upload_from_string(json.dumps(data), content_type="application/json")
-
-def main(event, context):
-    BASE_URL = "https://api.example.com/data"  # replace with actual
-    BUCKET_NAME = "your-data-bucket-name"  # replace with actual
-
-    data = fetch_paginated_data(BASE_URL)
-    upload_to_gcs(BUCKET_NAME, data)
+  storage_client = storage.Client()
+  bucket = storage_client.bucket(bucket_name)
+  date_str = datetime.now(timezone.utc).strftime('%Y-%m-%d')
+  blob = bucket.blob(f"data/{date_str}.json")
+  lines = [json.dumps(item) for item in data]
+  file_content = '\n'.join(lines)
+  blob.upload_from_string(file_content, content_type="application/json")
